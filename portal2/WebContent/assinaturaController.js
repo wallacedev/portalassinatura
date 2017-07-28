761083691b9c1b816e738579a9c9910f6d1758f8
wallacedev <wallaceat14@gmail.com>
2017-07-28 15:48:34 -0300

    Melhorias comportamento modal

--- portal2/WebContent/assinaturaController.js
+++ portal2/WebContent/assinaturaController.js
@@ -20,7 +20,6 @@ angular.module('portal').controller('assinatura', function($scope, $uibModal, $l
 	$scope.remove = function(index){				
 		$scope.linhas.splice( index, 1 );		
 	};
-	
 
 	$scope.fileNameChanged = function(ele) {
 		var tipo = ele.files[0].type;
@@ -42,70 +41,54 @@ angular.module('portal').controller('assinatura', function($scope, $uibModal, $l
 		$scope.$apply();
 
 	};
-
 	
 	$scope.assinar = function(){
 		alert("Chamar servico Java Rest para assinar");
-		
 	};
-	
-	$ctrl.politica = function (index, size, parentSelector) {
-		//$scope.linhas[index].filetype = type;
-		var parentElem = parentSelector ? 
-	      angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
-	    var modalInstance = $uibModal.open({
-	      animation: $ctrl.animationsEnabled,
-	      ariaLabelledBy: 'modal-title',
-	      ariaDescribedBy: 'modal-body',
-	      templateUrl: 'myModalContent.html',
-	      controller: 'ModalInstanceCtrl',
-	      controllerAs: '$ctrl',
-	      size: size,
-	      appendTo: parentElem,
-	      resolve: {
-	        items: function () {
-	          return $ctrl.items;
-	        },
-	        linha: function (){
-	          return $scope.linhas[index];	
-	        }
-	      }
-	    });
-	    
-	    modalInstance.result.then(function (selectedItem) {
-	        $ctrl.selected = selectedItem;
-	        
-	      }, function () {
-	        $log.info('Modal dismissed at: ' + new Date());
-	      });
-	 };
+
+	$ctrl.politica = function(index, size, parentSelector) {
+		var parentElem = parentSelector ? angular.element($document[0]
+				.querySelector('.modal-demo ' + parentSelector))
+				: undefined;
+
+		var modalInstance = $uibModal.open({
+			animation : $ctrl.animationsEnabled,
+			ariaLabelledBy : 'modal-title',
+			ariaDescribedBy : 'modal-body',
+			templateUrl : 'myModalContent.html',
+			controller : 'ModalInstanceCtrl',
+			controllerAs : '$ctrl',
+			size : size,
+			appendTo : parentElem,
+			resolve : {
+				linha : function() {
+					return $scope.linhas[index];
+				}
+			}
+		});
+
+		modalInstance.result.then(function(selectedItem) {
+			$ctrl.selected = selectedItem;
+
+		}, function() {
+			$log.info('Modal dismissed at: ' + new Date());
+		});
+	};
 		
 });
 
 angular.module('portal').controller('ModalInstanceCtrl',
-		function($scope, $uibModalInstance, items, linha) {
-
-			var $ctrl = this;
-			$ctrl.items = items;
-			$ctrl.selected = {
-				item : $ctrl.items[0]
-			};
+		function($scope, $uibModalInstance, linha) {
 
 			$scope.linha = linha;
-
 			
 			$scope.radiochange = function() {
 				$scope.linha.politicasubtipo = "padrao";
 			};
-			
-
-			//$scope.filename = linha.filecontent;
-			//$scope.filetype = linha.filetype;
 
 			$scope.controllcms = "";
 			$scope.controllxml = "";
 			$scope.controllpdf = "";
-
 			
 			if (linha.estado == "pdf") {
 				$scope.ctrldivxml = "disabled";
@@ -121,7 +104,6 @@ angular.module('portal').controller('ModalInstanceCtrl',
 				$scope.ctrldivpdf = "disabled";
 				$scope.ctrlradiopdf = true;
 			}
-			
 
 			$scope.ok = function() {
 				$uibModalInstance.close($ctrl.selected.item);
@@ -131,37 +113,3 @@ angular.module('portal').controller('ModalInstanceCtrl',
 				$uibModalInstance.dismiss('cancel');
 			};
 		});
-
-
-
-/*
-$(function() {
-
-	  // We can attach the `fileselect` event to all file inputs on the page
-	  $(document).on('change', ':file', function() {
-	    var input = $(this),
-	        numFiles = input.get(0).files ? input.get(0).files.length : 1,
-	        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-	    input.trigger('fileselect', [numFiles, label]);
-	  });
-
-	  // We can watch for our custom `fileselect` event like this
-	  $(document).ready( function() {
-	      $(':file').on('fileselect', function(event, numFiles, label) {
-
-	          var input = $(this).parents('.input-group').find(':text'),
-	              log = numFiles > 1 ? numFiles + ' files selected' : label;
-
-	          if( input.length ) {
-	              input.val(log);
-	          } else {
-	              if( log ) alert(log);
-	          }
-
-	      });
-	  });
-	  
-	});
-
-*/
-
